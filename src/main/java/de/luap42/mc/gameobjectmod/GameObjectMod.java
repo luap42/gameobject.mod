da9325a2155c3e4d67f7d326275b09a3e89bcb61
luap42 <pstrobach15+luap42@gmail.com>
2024-04-12 13:22:11 +0200

    Initialize

---
+++ src/main/java/de/luap42/mc/gameobjectmod/GameObjectMod.java
@@ -0,0 +1,17 @@
+package de.luap42.mc.gameobjectmod;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GameObjectMod extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
